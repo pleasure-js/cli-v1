1c15e1ae6e009f1ed5122c43e5029c57f54d290a
Martin Rafael Gonzalez <tin@devtin.io>
2019-09-25 12:26:15 -0400

    Revert "dist"
    This reverts commit 9b7ffdf1

--- index.js
+++ index.js
@@ -1,3 +1,2 @@
 const { printCommandsIndex } = require('./lib/print-commands-index.js')
-const nuxt = require('nuxt')
-module.exports = { printCommandsIndex, nuxt }
+module.exports = { printCommandsIndex }
